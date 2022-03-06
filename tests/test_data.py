b4663aea30cfc6f0fd5e0b7b26884455e172e8a8
Kranthi Reddy <vudu0001@ou.edu>
2022-03-06 19:46:22 +0000

    Made some Changes

---
+++ tests/test_data.py
@@ -0,0 +1,136 @@
+
+import pandas as pd
+import project0
+import PyPDF2
+import re
+import sqlite3
+
+
+def fetchincidentstest():
+    f = project0.fetchincidents(url)  
+    assert f is not None  
+
+def extractincidentstest():
+    f = project0.fetchincidents(url)
+    e = project0.extractincidents(f)
+
+    assert len(e) != 0
+    assert type(e) is list
+
+    for values in e:
+        assert len(values)==5  
+
+def createdbtest():
+    project0.createdb() 
+    conn = sqlite3.connect('normanpd.db')  
+    c = conn.cursor()  
+    c.execute('''select * From incidents''')  
+    assert c.fetchall()==[]  
+
+def populatedbtest():
+    f= project0.fetchIncidents(url)
+    e = project0.extractIncidents(f)
+    db = project0.createDB()
+    project0.populatedB(e)
+
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute('''select count(*) from incidents''')
+    assert len(e) == c.fetchall()[0][0]
+
+def extractedpdfdata():
+    pdfReader = PyPDF2.pdf.PdfFileReader('2022-02-27_daily_incident_summary.pdf')
+    pagenum = pdfReader.getNumPages()
+    #print("number of pages in the pdf",pagenum)
+
+    flist=[]
+
+    for i in range(pagenum):
+        page1 = pdfReader.getPage(i).extractText()
+        list1 = page1.split('\n')
+        #print(list1.count('\n'),end="--\n")
+        #print(list1)
+        if i==0:
+            list1 = list1[5:-3]
+        if i == pagenum-1:
+            list1= list1[:-2] 
+        flist.append(list1)
+    
+    flist1 = []
+
+    for k in range(0,len(flist)):
+        cnt = 0
+        temp=[]
+        #print("flist lenght: ",len(flist[k]))
+        i=0
+        while(i<=len(flist[k])-5):
+            #print(flist[k])
+            date_match = re.search(r'(\d+/\d+/\d+)',flist[k][i])
+            if(date_match!=None):
+                cnt = cnt +1
+                #adding to the temp list
+                temp.append(flist[k][i])
+                ino_match = re.search(r"2022-\w+", flist[k][i+1])
+
+                if(ino_match!=None):
+                    cnt = cnt+1
+                    #adding to the temp list
+                    temp.append(flist[k][i+1])
+                    io_match0 = re.search("^OK0", flist[k][i+2])
+                    io_match1 = re.search("^EMS",flist[k][i+2])
+                    io_match2 = re.search("^14005",flist[k][i+2])
+                    #print(io_match2)
+
+                    if(io_match0!=None or io_match1!=None or io_match2!=None):  
+                        cnt = cnt + 1
+                        #null values are present
+                        temp.append('NA')
+                        temp.append('NA')
+                        temp.append(flist[k][i+2])
+                        #print("NA insetion: ",temp)
+                    else:
+                        cnt = cnt + 3
+                        o_match0 = re.search("^OK0", flist[k][i+4])
+                        o_match1 = re.search("^EMS",flist[k][i+4])
+                        o_match2 = re.search("^14005",flist[k][i+4])
+
+                        # checking if 2nd column data is in newline
+                        if(o_match0==None and o_match1==None and o_match2==None):
+                            # if 2nd column data has new lines 
+                            # append 2nd and 3rd column as single column
+                            temp.append(flist[k][i+2]+flist[k][i+3])
+                            #append the remaining columns
+                            temp.append(flist[k][i+4])
+                            temp.append(flist[k][i+5])
+                        else:
+                            #there are no newlines in 2nd column(index 0)
+                            temp.append(flist[k][i+2])
+                            temp.append(flist[k][i+3])
+                            temp.append(flist[k][i+4]) 
+                        #print("normal insetion: ",temp)
+
+            if(cnt==5 or cnt==3):
+                i = cnt + i
+                #print(cnt)
+                flist1.append(temp)
+                cnt = 0
+                temp = []
+            else:
+                i = i+1
+    df=pd.DataFrame(flist1)
+    df.columns = ["Date / Time","Incident Number","Location", "Nature","Incident ORI"]
+    return df 
+
+def teststatus():
+    incidentslist = extractedpdfdata()
+    project0.populatedB(incidentslist)
+
+    conn = sqlite3.connect('norman.db')
+    c = conn.cursor()
+
+    c.execute('''select sum(count(*)) from 
+    (select nature, count(*) from incidents group by nature)''')
+
+    assert len(incidentslist) == c.fetchall()[0][0]
+
+url="https://www.normanok.gov/sites/default/files/documents/2022-02/2022-02-27_daily_incident_summary.pdf"
